a21226e007beea93cd5cb02540fe274b04ad5092
JorgeLuisBacVasquez <Juniorbacb@gmail.com>
2024-09-28 17:36:12 -0600

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,7 @@
+// Mostrar una alerta al cargar la página
+window.onload = function() {
+    alert('¡Bienvenido a AgroBovino, tu mejor opción en ganado y maquinaria agrícola!');
+    
+    // Mensaje opcional en la consola
+    console.log('Bienvenido al sitio web de AgroBovino. Aquí encontrarás los mejores productos agrícolas y ganaderos.');
+};
